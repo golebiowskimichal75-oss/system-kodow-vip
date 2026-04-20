3cb59c1b823e968aadccf4ac691411253944d896
golebiowskimichal75-oss <golebiowskimichal75@gmail.com>
2026-04-20 22:12:23 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+let ADMIN_PASSWORD = localStorage.getItem('adminPassword') || "admin123";
+const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1495873957574873133/-r54lqxF_YnWd_VVKbkoPW-RLZfcpKysJl6JusjBQTjQgBEsk1AZUlEVZNTJ3_hSD0Sl";
+
+function checkLogin() {
+    if (document.getElementById('adminPass').value === ADMIN_PASSWORD) {
+        document.getElementById('login-section').style.display = "none";
+        document.getElementById('admin-section').style.display = "block";
+        updateUI();
+    } else { alert("Błędne hasło!"); }
+}
+
+function generateCode() {
+    let newCode = "";
+    for(let i=0; i<16; i++) newCode += Math.floor(Math.random()*10);
+    let rank = document.getElementById('codeRank').value;
+    let codes = JSON.parse(localStorage.getItem('activeCodesRanked')) || [];
+    codes.push({ key: newCode, type: rank });
+    localStorage.setItem('activeCodesRanked', JSON.stringify(codes));
+    document.getElementById('codeDisplay').innerText = newCode;
+    updateUI();
+}
+
+function updateUI() {
+    let codes = JSON.parse(localStorage.getItem('activeCodesRanked')) || [];
+    document.getElementById('codeCounter').innerText = codes.length;
+    let history = "";
+    codes.slice(-3).reverse().forEach(c => history += c.key + " [" + c.type + "]<br>");
+    document.getElementById('historyList').innerHTML = history;
+}
+
+function redeemCode() {
+    let nick = document.getElementById('discordNick').value;
+    let input = document.getElementById('userCode').value;
+    let codes = JSON.parse(localStorage.getItem('activeCodesRanked')) || [];
+    let idx = codes.findIndex(c => c.key === input);
+
+    if (nick && idx !== -1) {
+        let activated = codes[idx];
+        fetch(DISCORD_WEBHOOK, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ content: `🚀 **AKTYWACJA!**\n**Użytkownik:** ${nick}\n**Ranga:** ${activated.type}` })
+        });
+        codes.splice(idx, 1);
+        localStorage.setItem('activeCodesRanked', JSON.stringify(codes));
+        confetti({ particleCount: 150, spread: 70 });
+        document.getElementById('test-section').style.display = "none";
+        document.getElementById('premium-content').style.display = "block";
+        document.getElementById('rankInfo').innerText = "Ranga: " + activated.type;
+    } else { alert("Błędny kod lub brak nicku!"); }
+}
+
+function logout() { location.reload(); }
+function changeAdminPassword() {
+    let p = document.getElementById('newPassInput').value;
+    if(p.length > 3) { 
+        ADMIN_PASSWORD = p; 
+        localStorage.setItem('adminPassword', p); 
+        alert("Hasło zmienione!"); 
+        logout(); 
+    }
+}
